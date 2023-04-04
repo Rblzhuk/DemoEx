f494e96911ff2d46854953b3127ca4daa334e134
Студент <Student@College.edu>
2023-04-04 18:30:42 +0300

    first

---
+++ Windows/Authorization.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Timers;
+
+namespace CaptchaInWpf.Windows
+{
+    /// <summary>
+    /// Interaction logic for Authorization.xaml
+    /// </summary>
+    public partial class Authorization : Window
+    {
+        Timer timer;
+        private bool isTimerCouting = false;
+        public Authorization()
+        {
+            InitializeComponent();
+            StackPanel_CapchaBlock.Visibility = Visibility.Collapsed;
+        }
+
+        private void Button_Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isTimerCouting)
+            {
+                DataBase.User user = SourceCore.MyBase.User.SingleOrDefault(usr => usr.UserLogin == TextBox_Login.Text && usr.UserPassword == TextBox_Password.Password);
+                if (user != null)
+                {
+                    if (StackPanel_CapchaBlock.Visibility == Visibility.Visible)
+                    {
+                        if (TextBox_Capcha.Text != TextBox_UserCapcha.Text)
+                        {
+                            MessageBox.Show("Неверно введена капча");
+                        }
+                    }
+                    else
+                    {
+                        //MessageBox.Show($"Здравствуйте {user.UserName}");
+                        MainWindow mainWindow = new MainWindow();
+                        mainWindow.Show();
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Неверный логин или пароль");
+                    if (StackPanel_CapchaBlock.Visibility == Visibility.Visible)
+                    {
+                        timer = new Timer(10000);
+                        timer.Elapsed += StopTimer;
+                        timer.Start();
+                        isTimerCouting = true;
+
+                    }
+                    StackPanel_CapchaBlock.Visibility = Visibility.Visible;
+                }
+            }
+            else
+            {
+                MessageBox.Show("От неудачной попытки должно пройти 10 секунд");
+            }
+        }
+
+        private void Button_Registration_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.Registration reg = new Registration();
+            reg.Show();
+            this.Close();
+        }
+
+        private void Button_GenerateCapcha_Click(object sender, RoutedEventArgs e)
+        {
+            String allowchar = " ";
+            allowchar = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+            allowchar += "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,y,z";
+            allowchar += "1,2,3,4,5,6,7,8,9,0";
+            //разделитель
+            char[] a = { ',' };
+            //расщепление массива по разделителю
+            String[] ar = allowchar.Split(a);
+            String pwd = "";
+            string temp = "";
+            Random r = new Random();
+            for (int i = 0; i < 6; i++)
+            {
+                temp = ar[(r.Next(0, ar.Length))];
+                pwd += temp;
+            }
+            TextBox_Capcha.Text = pwd;
+        }
+
+        private void StopTimer(object sender, ElapsedEventArgs args)
+        {
+            timer.Stop();
+            isTimerCouting = false;
+        }
+    }
+}
---
+++ Windows/Registration.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CaptchaInWpf.Windows
+{
+    /// <summary>
+    /// Interaction logic for Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+
+        public Registration()
+        {
+            InitializeComponent();
+            DataContext = this;
+            ComboBox_Role.ItemsSource = SourceCore.MyBase.Role.ToList();
+            ComboBox_Role.SelectedIndex = 0;
+        }
+        
+
+        private void Button_BackToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Windows.Authorization authorization = new Authorization();
+            authorization.Show();
+            this.Close();
+        }
+
+        private void Button_Registration_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsDataValid())
+            {
+                DataBase.User user = new DataBase.User()
+                {
+                    UserLogin = TextBox_Login.Text,
+                    UserPassword = TextBox_Password.Password,
+                    UserName=TextBox_Name.Text,
+                    UserSurname=TextBox_Surname.Text,
+                    UserPatronymic = TextBox_Patronymic.Text,
+                    UserRole = ComboBox_Role.SelectedIndex,
+                };
+                SourceCore.MyBase.User.Add(user);
+                SourceCore.MyBase.SaveChanges();
+
+                Windows.Authorization authorization = new Authorization();
+                authorization.Show();
+                this.Close();
+            }
+        }
+
+        private bool IsDataValid()
+        {
+            Regex regex;
+            const string pat_login = "^[A-Za-z]{5,20}$";
+            const string pat_password = "^[A-Za-z0-9]{5,20}$";
+            const string pat_name = "^[A-Za-z]{5,20}$";
+            const string pat_surname = "^[A-Za-z]{5,20}$";
+            const string pat_patronymic = "^[A-Za-z]{0,20}$";
+
+            regex = new Regex(pat_login);
+            if (!regex.IsMatch(TextBox_Login.Text))
+            {
+                MessageBox.Show("Логин имеет неверный формат");
+                return false;
+            }
+            regex = new Regex(pat_password);
+            if (!regex.IsMatch(TextBox_Password.Password))
+            {
+                MessageBox.Show("Пароль имеет неверный формат");
+                return false;
+            }
+            if (TextBox_Password.Password != TextBox_RepeatPassword.Password)
+            {
+                MessageBox.Show("Пароли не совпадают");
+                return false;
+            }
+            regex = new Regex(pat_name);
+            if (!regex.IsMatch(TextBox_Name.Text))
+            {
+                MessageBox.Show("Имя имеет неверный формат");
+                return false;
+            }
+            regex = new Regex(pat_surname);
+            if (!regex.IsMatch(TextBox_Surname.Text))
+            {
+                MessageBox.Show("Фамилия имеет неверный формат");
+                return false;
+            }
+            regex = new Regex(pat_patronymic);
+            if (!regex.IsMatch(TextBox_Patronymic.Text))
+            {
+                MessageBox.Show("Отчество имеет неверный формат");
+                return false;
+            }
+
+            MessageBox.Show("Регистрация успешно завершена");
+
+            return true;
+        }
+    }
+}
